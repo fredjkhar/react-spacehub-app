57fa36bbb234baa381e87b4c007a1bb31397d2c0
skyByt <fredjkharroubi@gmail.com>
2022-07-21 05:30:14 -0400

    Minor changes

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -4,11 +4,11 @@ import Header from '../../components/Header'
 import Body from '../../components/Body'
 import Footer from '../../components/Footer'
 
-const Home = ({user, changeLanguage}) => {
+const Home = ({user, changeLanguage, setUser}) => {
   return (
     <>
       <Header user = {user} changeLanguage = {changeLanguage}/>
-      <Body changeLanguage = {changeLanguage}/>
+      <Body changeLanguage = {changeLanguage} setUser = {setUser}/>
       <Footer changeLanguage = {changeLanguage}/>
     </>
   )
